3ef3dfafa188941912bdd910712ca31ffd0b5107
AmeriQ <AmeriQ4top@users.noreply.github.com>
2020-02-07 11:29:42 -0500

    Сложность / инструменты
    Изменение рейтов, количества необходимого опыта для прокачки ур. проф. и увеличение прочности инструментов выше тира.

--- Mods/AutoGen/Tool/ModernHoe.cs
+++ Mods/AutoGen/Tool/ModernHoe.cs
@@ -56,7 +56,7 @@ namespace Eco.Mods.TechTree
         public override IDynamicValue SkilledRepairCost { get { return skilledRepairCost; } }
 
 
-        public override float DurabilityRate { get { return DurabilityMax / 1500f; } }
+        public override float DurabilityRate { get { return DurabilityMax / 7500f; } }
         
         public override Item RepairItem         {get{ return Item.Get<SteelItem>(); } }
         public override int FullRepairAmount    {get{ return 15; } }
