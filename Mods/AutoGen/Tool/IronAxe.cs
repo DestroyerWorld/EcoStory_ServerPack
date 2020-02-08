00e8f78dbd0ada61ee5d3186a5526207f3fe980e
DestroyerWorld <58342391+DestroyerWorld@users.noreply.github.com>
2020-02-08 23:32:26 +0300

    Испровление ошибок запуска
    Исправлена загрузка, но потерены настройки рейтов и настройки предметов

--- Mods/AutoGen/Tool/IronAxe.cs
+++ Mods/AutoGen/Tool/IronAxe.cs
@@ -58,7 +58,7 @@ namespace Eco.Mods.TechTree
         public override IDynamicValue SkilledRepairCost { get { return skilledRepairCost; } }
 
 
-        public override float DurabilityRate { get { return DurabilityMax / 2500f; } }
+        public override float DurabilityRate { get { return DurabilityMax / 750f; } }
         
         public override Item RepairItem         {get{ return Item.Get<IronIngotItem>(); } }
         public override int FullRepairAmount    {get{ return 8; } }
--- Mods/AutoGen/Tool/ModernHammer.cs
+++ Mods/AutoGen/Tool/ModernHammer.cs
@@ -56,7 +56,7 @@ namespace Eco.Mods.TechTree
         public override IDynamicValue SkilledRepairCost { get { return skilledRepairCost; } }
 
 
-        public override float DurabilityRate { get { return DurabilityMax / 10000f; } }
+        public override float DurabilityRate { get { return DurabilityMax / 2000f; } }
         
         public override Item RepairItem         {get{ return Item.Get<SteelItem>(); } }
         public override int FullRepairAmount    {get{ return 15; } }
