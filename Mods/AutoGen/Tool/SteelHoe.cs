ec2d128697144beede9bee2bcdb886a5a2011f08
DestroyerWorld <58342391+DestroyerWorld@users.noreply.github.com>
2020-02-09 17:52:26 +0300

    Востановлен баланс
    Баланс востановлен до параметров которые были до их потери.

--- Mods/AutoGen/Tool/SteelHoe.cs
+++ Mods/AutoGen/Tool/SteelHoe.cs
@@ -56,7 +56,7 @@ namespace Eco.Mods.TechTree
         public override IDynamicValue SkilledRepairCost { get { return skilledRepairCost; } }
 
 
-        public override float DurabilityRate { get { return DurabilityMax / 750f; } }
+        public override float DurabilityRate { get { return DurabilityMax / 3500f; } }
         
         public override Item RepairItem         {get{ return Item.Get<SteelItem>(); } }
         public override int FullRepairAmount    {get{ return 8; } }
